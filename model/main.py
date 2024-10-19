198adfc0a53fbd4acd8b067162888a5b0db2a7e6
biacosta7 <contact.beatrizcosta@gmail.com>
2024-10-19 10:54:19 -0300

    data cleaning

--- model/main.py
+++ model/main.py
@@ -2,11 +2,16 @@ import pandas as pd
 
 def get_clean_data():
     data = pd.read_csv("data\data-wisconsin.csv")
-    print(data.head())
+
+    data = data.drop(['Unnamed: 32', 'id'], axis=1)
+
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
+
     return data
 
 def main():
     data = get_clean_data()
+    print(data.head())
 
 if __name__ == '__main__':
     main()
