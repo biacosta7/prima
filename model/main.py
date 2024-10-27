1a3d37eae9838711d769113fa7d60b60177f78da
biacosta7 <contact.beatrizcosta@gmail.com>
2024-10-27 11:36:23 -0300

    Testando model

--- model/main.py
+++ model/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 def create_model(data):
     x = data.drop(['diagnosis'], axis=1)
@@ -18,11 +19,16 @@ def create_model(data):
     model = LogisticRegression()
     model.fit(x_train, y_train)
 
+    #testar model
+    y_prediction = model.predict(x_test)
+    print("Acurracy: ", accuracy_score(y_test, y_prediction))
+    print("Classification report:\n", classification_report(y_test, y_prediction))
+
     return model, scaler
 
 
 def get_clean_data():
-    data = pd.read_csv("data\data-wisconsin.csv")
+    data = pd.read_csv("../data/data-wisconsin.csv")
 
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
 
